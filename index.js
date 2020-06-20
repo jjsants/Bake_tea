2a094de2bf0927d21b0224b6f44b8b734577db2f
Edivar O Ceadaigh <junior_scan16@hotmail.com>
2020-06-20 21:25:31 +0000

    added javascript code to fade out website name when scrolling down. fixed image position from the intro page on mobile mode. added new image on collections page. added a smooth moving when clicking on the nav menu options. made the menu bar fixed on computer screen, added background with shadow and opacity.

--- index.js
+++ index.js
@@ -1,97 +1,99 @@
 
-
+$(window).scroll(function(){
+  $(".logo").css("opacity", 1 - $(window).scrollTop() / 250);
+});
 
 
 // using fullpage.js to structure the scrolling style of the page
-new fullpage('#fullpage', {
-  autoScrolling: false,
-  navigation: true,
-  navigationTooltips: ["home", "about us", "collection", "testimonials", "recipes"],
-  showActiveTooltip: false,
-  // controlArrows:true,
+// new fullpage('#fullpage', {
+  // autoScrolling: false,
+  // navigation: true,
+  // navigationTooltips: ["home", "about us", "collection", "testimonials", "recipes"],
+  // showActiveTooltip: false,
+  // // controlArrows:true,
   // slidesNavigation:true
 
-  onLeave: function (origin, destination, direction) {
-    var leavingSection = this;
-    var tl = gsap.timeline({
-      delay: 0.7
-    });
-    //after leaving section 2
-    if (origin.index == 0 && direction == 'down') {
-      //Used greeshock 3 to do the animations.
-      tl.fromTo(".presentation", {
-        height: 0,
-        width: 0
-      }, {
-        duration: 1,
-        width: "50%",
-        height: "70%",
-        ease: "power4",
-        right: "25%"
-      })
-        .from(".intro-text", {
-          opacity: 0,
-          duration: 2,
-          ease: "power4"
-        })
-        .from(".hidden-intro", {
-          opacity: 0,
-          visibility: "visible",
-          duration: 1,
-          x: 200,
-          ease: "power4"
-        }, "-=2")
-        .to(".hidden-intro", {
-          opacity: 100,
-          visibility: "visible",
-          x: 0
-        })
-    } else if (origin.index == 1 && direction == 'down') {
-      // alert("Going to section 2!");
+//   onLeave: function (origin, destination, direction) {
+//     var leavingSection = this;
+//     var tl = gsap.timeline({
+//       delay: 0.7
+//     });
+//     //after leaving section 2
+//     if (origin.index == 0 && direction == 'down') {
+//       //Used greeshock 3 to do the animations.
+//       tl.fromTo(".presentation", {
+//         height: 0,
+//         width: 0
+//       }, {
+//         duration: 1,
+//         width: "50%",
+//         height: "70%",
+//         ease: "power4",
+//         right: "25%"
+//       })
+//         .from(".intro-text", {
+//           opacity: 0,
+//           duration: 2,
+//           ease: "power4"
+//         })
+//         .from(".hidden-intro", {
+//           opacity: 0,
+//           visibility: "visible",
+//           duration: 1,
+//           x: 200,
+//           ease: "power4"
+//         }, "-=2")
+//         .to(".hidden-intro", {
+//           opacity: 100,
+//           visibility: "visible",
+//           x: 0
+//         })
+//     } else if (origin.index == 1 && direction == 'down') {
+//       // alert("Going to section 2!");
 
-    } else if (origin.index == 2 && direction == 'down') {
-      // alert("Going to section 3!");
+//     } else if (origin.index == 2 && direction == 'down') {
+//       // alert("Going to section 3!");
 
-    }
-    // up
-    else if (origin.index == 3 && direction == 'up') {
-      // alert("Going to section 2!");
+//     }
+//     // up
+//     else if (origin.index == 3 && direction == 'up') {
+//       // alert("Going to section 2!");
 
-    } else if (origin.index == 2 && direction == 'up') {
-      //alert("Going to section 1!");
+//     } else if (origin.index == 2 && direction == 'up') {
+//       //alert("Going to section 1!");
 
-    } else if (origin.index == 1 && direction == 'up') {
-      //alert("Going to section 0!");
-      tl.fromTo(".presentation", {
-        height: 0,
-        width: 0
-      }, {
-        duration: 1,
-        width: "50%",
-        height: "70%",
-        ease: "power4",
-        right: "25%"
-      })
-        .from(".intro-text", {
-          opacity: 0,
-          duration: 2,
-          ease: "power4"
-        })
-        .from(".hidden-intro", {
-          opacity: 0,
-          visibility: "visible",
-          duration: 1,
-          x: 200,
-          ease: "power4"
-        }, "-=2")
-        .to(".hidden-intro", {
-          opacity: 100,
-          visibility: "visible",
-          x: 0
-        })
-    }
-  }
-});
+//     } else if (origin.index == 1 && direction == 'up') {
+//       //alert("Going to section 0!");
+//       tl.fromTo(".presentation", {
+//         height: 0,
+//         width: 0
+//       }, {
+//         duration: 1,
+//         width: "50%",
+//         height: "70%",
+//         ease: "power4",
+//         right: "25%"
+//       })
+//         .from(".intro-text", {
+//           opacity: 0,
+//           duration: 2,
+//           ease: "power4"
+//         })
+//         .from(".hidden-intro", {
+//           opacity: 0,
+//           visibility: "visible",
+//           duration: 1,
+//           x: 200,
+//           ease: "power4"
+//         }, "-=2")
+//         .to(".hidden-intro", {
+//           opacity: 100,
+//           visibility: "visible",
+//           x: 0
+//         })
+//     }
+//   }
+// });
 
 //collection fullPage
 //function used to create the first carousel
@@ -256,7 +258,6 @@ content.appendChild(searchNoInput)
 searchButton.addEventListener("click", () => {
   console.log("button pressed")
 
-  // debugger;
   if (searchInput.value == "") {
     setTimeout(function () {
       searchNoInput.innerHTML = "<h2> Please enter a name.</h2>" // only working once at the moment.
